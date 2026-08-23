0d13f1c62d9b1df3aa3ee8df246569960b26f20d
Aryanlivi <aryanlividev@gmail.com>
2026-08-23 15:25:45 +0545

    fix deploy to pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typedRoutes: true,
+  output: "export",
+  basePath: "/aryan-manandhar",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
